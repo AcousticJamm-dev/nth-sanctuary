ab18e60b7b3c7f30b2131767f46447ada41a08f9
name = theLucidLibrarian <alexanderhaejunkim@gmail.com>
2026-01-22 00:33:56 -0800

    Added new tileset for climbing sections

--- scripts/world/tilesets/bg_dw_church_tileset_new.tsx
+++ scripts/world/tilesets/bg_dw_church_tileset_new.tsx
@@ -1,4 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="bg_dw_church_tileset_new" tilewidth="40" tileheight="40" tilecount="582" columns="6">
+<tileset version="1.10" tiledversion="1.11.2" name="bg_dw_church_tileset_new" tilewidth="40" tileheight="40" tilecount="582" columns="6">
  <image source="../../../assets/sprites/tilesets/dr ch3-4 fixed tilesets/bg_dw_church_tileset_new.png" width="240" height="3880"/>
+ <tile id="570" probability="0.25"/>
+ <tile id="576" probability="0.75"/>
 </tileset>

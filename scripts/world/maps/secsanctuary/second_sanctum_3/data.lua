5f5f099b7374597215a842dc851bf6c9437ae1bb
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-01-22 17:54:11 -0700

    Small change

--- scripts/world/maps/secsanctuary/second_sanctum_3/data.lua
+++ scripts/world/maps/secsanctuary/second_sanctum_3/data.lua
@@ -1,7 +1,7 @@
 return {
   version = "1.10",
   luaversion = "5.1",
-  tiledversion = "1.11.2",
+  tiledversion = "1.10.2",
   class = "",
   orientation = "orthogonal",
   renderorder = "right-down",
@@ -35,8 +35,7 @@ return {
     {
       name = "ExtraClimbableBrickTiles",
       firstgid = 814,
-      filename = "../../../tilesets/ExtraClimbableBrickTiles.tsx",
-      exportfilename = "../../../tilesets/ExtraClimbableBrickTiles.tsx"
+      filename = "../../../tilesets/ExtraClimbableBrickTiles.tsx"
     }
   },
   layers = {

ec780b2851825229e2832111e68c4524b48ccd61
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-02-04 17:22:52 -0700

    Fast travel last map flag fix and improvement

--- scripts/hooks/Map.lua
+++ scripts/hooks/Map.lua
@@ -2,13 +2,11 @@ local Map, super = Class("Map", true)
 
 function Map:onExit()
 	super.onExit(self)
-	if Game.world.map.id and 
-	Game.world.map.id ~= "fast_travel" and 
-	Game.world.map.id ~= "grand_door" and 
-	Game.world.map.id ~= "citadel_room" and
-	Game.world.map.id ~= "citadel_room2" and
-	Game.world.map.id ~= "citadel_room3" and
-	Game.world.map.id ~= "Map" then
+	if Game.world.map.id and not TableUtils.contains({
+		"fast_travel", "grand_door", "citadel_room",
+		"citadel_room/citadel_room2", "citadel_room/citadel_room3",
+		"Map"
+	}, Game.world.map.id) then
 		Game:setFlag("ft_last_map", Game.world.map.id)
 	end
 end

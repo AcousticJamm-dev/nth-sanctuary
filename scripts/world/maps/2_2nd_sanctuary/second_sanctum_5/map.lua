a6e40c7895b6471cad9b94480bc9b004f00dc98c
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-03-06 00:13:30 -0700

    Set rotating tower angle properly when entering from bottom transitions

--- scripts/world/maps/2_2nd_sanctuary/second_sanctum_5/map.lua
+++ scripts/world/maps/2_2nd_sanctuary/second_sanctum_5/map.lua
@@ -3,6 +3,9 @@ local map, super = Class(Map, "second_sanctum_5")
 
 function map:onEnter()
 	self.world.camera:setBounds(0, 0, 30 * self.tile_width, self.height * self.tile_height)
+	if self.world.player.y >= ((self.height * self.tile_height) / 2) then
+		self.cyltower.tower_angle = 278.59
+	end
 end
 
 function map:onExit()

3105f1f2c4358333144f666644227c7d0a0caeb0
name = theLucidLibrarian <alexanderhaejunkim@gmail.com>
2026-03-09 15:22:25 -0700

    Added tileset for brittle towerclimb tiles

---
+++ scripts/world/tilesets/ExtraTowerTiles.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="ExtraTowerTiles" tilewidth="40" tileheight="40" tilecount="6" columns="3">
+ <editorsettings>
+  <export target="ExtraTowerTiles.lua" format="lua"/>
+ </editorsettings>
+ <image source="../../../assets/sprites/tilesets/dr ch3-4 fixed tilesets/ExtraTowerTiles.png" width="120" height="80"/>
+</tileset>

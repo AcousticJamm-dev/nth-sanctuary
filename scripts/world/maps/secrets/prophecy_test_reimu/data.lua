ec94b04fd230afc5860615cac7b96aea5504c46d
name = theLucidLibrarian <alexanderhaejunkim@gmail.com>
2026-04-01 01:25:37 -0700

    Reimu prophecy joke

---
+++ scripts/world/maps/secrets/prophecy_test_reimu/data.lua
@@ -0,0 +1,108 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.12.1",
+  class = "",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 16,
+  height = 12,
+  tilewidth = 40,
+  tileheight = 40,
+  nextlayerid = 3,
+  nextobjectid = 11,
+  properties = {
+    ["border"] = "church_grand"
+  },
+  tilesets = {
+    {
+      name = "libraryexcerpt",
+      firstgid = 1,
+      filename = "../../../tilesets/libraryexcerpt.tsx",
+      exportfilename = "../../../tilesets/libraryexcerpt.lua"
+    },
+    {
+      name = "bg_dw_church_2_tileset",
+      firstgid = 49,
+      filename = "../../../tilesets/bg_dw_church_2_tileset.tsx"
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      x = 0,
+      y = 0,
+      width = 16,
+      height = 12,
+      id = 1,
+      name = "Tile Layer 1",
+      class = "",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      }
+    },
+    {
+      type = "objectgroup",
+      draworder = "topdown",
+      id = 2,
+      name = "objects",
+      class = "",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      objects = {
+        {
+          id = 1,
+          name = "prophecy",
+          type = "",
+          shape = "point",
+          x = 320,
+          y = 240,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          opacity = 1,
+          visible = true,
+          properties = {
+            ["alwaysvisible"] = true,
+            ["fade_edges"] = true,
+            ["no_back"] = true,
+            ["offx"] = 0,
+            ["offy"] = 0,
+            ["panel_h"] = 140,
+            ["panel_w"] = 160,
+            ["spr_offx"] = 64,
+            ["spr_offy"] = 78,
+            ["text"] = "A WITCH, FLYING TOWARDS SCARLET MANSION.",
+            ["texture"] = "reimu",
+            ["txt_offx"] = -157,
+            ["txt_offy"] = -30
+          }
+        }
+      }
+    }
+  }
+}

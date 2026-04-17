2f6305cc0f0b0ca61b38ef46e39686600a552d12
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-04-17 03:02:06 -0700

    Update jellies.lua

--- scripts/battle/waves/jellycruel/jellies.lua
+++ scripts/battle/waves/jellycruel/jellies.lua
@@ -5,12 +5,15 @@ function Basic:init()
     self:setArenaSize(150, 50)
 	self.attacks = 0
     self.wa = love.math.random(1, 5)
+	self.arena_ideal_x = 0
+	self.arena_timer = 30
 end
 
 function Basic:onStart()
     -- Every 0.33 seconds...
     local arena = Game.battle.arena
     self.arena_start_x = arena.x
+	self.arena_ideal_x = self.arena_start_x + math.sin(Kristal.getTime()*3)*25
     self.timer:every(0.5, function()
 		self.attacks = self.attacks + 1
 		local x = MathUtils.random(Game.battle.arena.left, Game.battle.arena.right)
@@ -24,7 +27,9 @@ end
 function Basic:update()
     -- Code here gets called every frame
     if self.wa == 3 then
-        Game.battle.arena:setPosition(self.arena_start_x + math.sin(Kristal.getTime()*3)*25, Game.battle.arena.y)
+		self.arena_timer = MathUtils.approach(self.arena_timer, 0, DTMULT)
+		self.arena_ideal_x = self.arena_start_x + math.sin(Kristal.getTime()*3)*25
+        Game.battle.arena:setPosition(MathUtils.lerp(self.arena_ideal_x, self.arena_start_x, self.arena_timer/30), Game.battle.arena.y)
     end
     super.update(self)
 end

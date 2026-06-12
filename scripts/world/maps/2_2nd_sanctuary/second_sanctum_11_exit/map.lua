1a8a7956c6dfb79a96696b8542d978c57659a083
Diamond Deltahedron <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-06-12 14:33:07 -0700

    Update map.lua

--- scripts/world/maps/2_2nd_sanctuary/second_sanctum_11_exit/map.lua
+++ scripts/world/maps/2_2nd_sanctuary/second_sanctum_11_exit/map.lua
@@ -4,7 +4,7 @@ local map, super = Class(Map, "second_sanctum_11_exit")
 function map:onEnter()
     super.onEnter(self)
 	for _, follower in ipairs(Game.world.followers) do
-		follower.visible = false
+		follower:remove()
 	end
 end
 

b7df6531bbcd69aec96878b033362d1fc944dd22
Diamond Deltahedron <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-07-10 01:22:27 -0700

    Update kris.lua

--- scripts/data/actors/kris.lua
+++ scripts/data/actors/kris.lua
@@ -4,12 +4,12 @@ local actor, super = Class("kris", true)
 function actor:init()
     super.init(self)
     TableUtils.merge(self.animations, {
-        ["slide"] = {"slide_animated", 4/30, true},
         ["pirouette"] = {"battle/pirouette", 3/30, true},
         ["fall_hurt"] = {"fall_hurt", 0, true},
         ["fall_hurt_wind"] = {"fall_hurt_wind", 1/5, true},
         ["run"] = {"run", 3/30, true}
     })
+    self.animations["slide"] = {"slide_animated", 4/30, true}
 end
 
 return actor

021c6504735b7f7e210a90e99a23814b1fa8e85a
Diamond Deltahedron <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-07-09 23:53:26 -0700

    Update kris.lua

--- scripts/data/party/kris.lua
+++ scripts/data/party/kris.lua
@@ -1,3 +1,14 @@
 local character, super = Class("kris", true)
 
+function character:init()
+    super.init(self)
+
+    self.stats = {
+        health = 201,
+        attack = 17,
+        defense = 2,
+        magic = 0
+    }
+end
+
 return character

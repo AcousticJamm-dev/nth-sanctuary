9976bb8b514529ba114213d303aadb1075ab3e1c
Diamond Deltahedron <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-07-10 00:49:59 -0700

    Update map.lua

--- scripts/world/maps/3_flooded_sanctuary/flooded_1/map.lua
+++ scripts/world/maps/3_flooded_sanctuary/flooded_1/map.lua
@@ -46,6 +46,7 @@ function map:onEnter()
         self.tiles4 = Game.world.map:getTileLayer("tiles4")
         self.tiles5 = Game.world.map:getTileLayer("tiles5")
         self.tiles6 = Game.world.map:getTileLayer("tiles6")
+        self.tiles0.alpha = 0
         self.tiles.alpha = 0
         self.tiles2.alpha = 0
         self.tiles3.alpha = 0

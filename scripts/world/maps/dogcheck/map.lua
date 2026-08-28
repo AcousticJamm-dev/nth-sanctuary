54f87d238af6262bb693a82fe205efa63853df88
hyperfae <hyperboid@proton.me>
2026-08-28 07:22:04 -0400

    Dogcheck camera fix

--- scripts/world/maps/dogcheck/map.lua
+++ scripts/world/maps/dogcheck/map.lua
@@ -8,8 +8,19 @@ end
 
 function map:onEnter()
 	local cam = Game.world.camera
-	Game.world.timer:tween(0, cam, {zoom_y = 2, zoom_x = 2, ox = -160, oy = 120}, 'linear')
+    cam.zoom_x = 2
+    cam.zoom_y = 2
+    cam.ox = -160
+    cam.oy = 120
 	--_prophecy = instance_create(camerax() + 160, cameray() + 120, 115);
+end
+
+function map:onExit()
+	local cam = Game.world.camera
+    cam.zoom_x = 1
+    cam.zoom_y = 1
+    cam.ox = 0
+    cam.oy = 0
 end
 
 return map

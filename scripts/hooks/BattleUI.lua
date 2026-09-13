ba9cd46b01a9af08b782f26ae07818cb8126f3d2
Diamond Deltahedron <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-09-12 23:05:57 -0700

    Update BattleUI.lua

--- scripts/hooks/BattleUI.lua
+++ scripts/hooks/BattleUI.lua
@@ -70,8 +70,8 @@ function BattleUI:draw()
             local head = Assets.getTexture(party.chara:getHeadIcons().."/head_alpha") or Assets.getTexture(party.chara:getHeadIcons().."/head")
             local name = Assets.getTexture(party.chara:getNameSprite())
             Draw.setColor(0,0,0,1)
-            Draw.draw(head, 16, 11 + 30*(k-1))
-            Draw.draw(name, 66, 16 + 30*(k-1))
+            Draw.draw(head, 17, 12 + 30*(k-1))
+            Draw.draw(name, 67, 17 + 30*(k-1))
 			
             Draw.setColor(1,1,1,1)
 			if party.chara.id == "lobby_man" then
@@ -85,7 +85,7 @@ function BattleUI:draw()
             Draw.draw(name, 65, 15 + 30*(k-1))
 			
             Draw.setColor(0,0,0,1)
-            Draw.rectangle("fill", 141, (30*(k-1))+name:getHeight()+4, 100, 10)
+            Draw.rectangle("fill", 142, (30*(k-1))+name:getHeight()+5, 100, 10)
 			local health_bg_col = PALETTE["action_health_bg"]
 			if party.chara.id == "lobby_man" then
 				health_bg_col = COLORS.dkgray
@@ -123,10 +123,10 @@ function BattleUI:draw()
             local health_offset = (#tostring(party.chara:getHealth()) - 1) * 8
 
             Draw.setColor(0, 0, 0, 1)
-            love.graphics.print(party.chara:getHealth(), 251, h + 1)
-            love.graphics.print("/", (261+health_offset), h + 1)
+            love.graphics.print(party.chara:getHealth(), 252, h + 2)
+            love.graphics.print("/", (262+health_offset), h + 2)
             local string_width = g:getWidth(tostring(party.chara:getStat("health")))
-            love.graphics.print(party.chara:getStat("health"), (281 + health_offset), h + 1)
+            love.graphics.print(party.chara:getStat("health"), (282 + health_offset), h + 2)
 			
             Draw.setColor(color[1], color[2], color[3])
             love.graphics.print(party.chara:getHealth(), 250, h)
